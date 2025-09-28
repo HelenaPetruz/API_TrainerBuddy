409694d5d64b7be2d7a8e0c0154f49c3015c20b7
Helena <helena.petruz@gmail.com>
2025-09-28 12:25:06 -0300

    Add files via upload

---
+++ src/main/java/com/example/api_TrainerBuddy/ApiTrainerBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_TrainerBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrainerBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTrainerBuddyApplication.class, args);
+	}
+
+}
